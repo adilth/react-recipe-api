53fc74533d3bb2798785597c3cecc5d20a392f11
adil tahiri <rajaadil1995@gmail.com>
2022-12-06 23:37:50 +0100

    add searched page route

---
+++ src/pages/Searched.jsx
@@ -0,0 +1,68 @@
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import styled from "styled-components";
+
+function Searched() {
+  const [searchRec, setSearchRec] = useState([]);
+
+  const params = useParams();
+
+  const getSearched = async (name) => {
+    const res = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_POON_API_KEY}&query=${name}`
+    );
+    const data = await res.json();
+    setSearchRec(data.results);
+    console.log(data.recipes);
+  };
+  useEffect(() => {
+    getSearched(params.search);
+  }, [params.search]);
+  return (
+    <Grid>
+      {searchRec.map((item) => {
+        return (
+          <Card key={item.id}>
+            <Link to={`/recipe/${item.id}`}>
+              <img src={item.image} alt={item.title} />
+              <h4>{item.title}</h4>
+              <p>See ingredients</p>
+            </Link>
+          </Card>
+        );
+      })}
+    </Grid>
+  );
+}
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(14rem, 1fr));
+  grid-auto-rows: 1fr;
+  gap: 2rem;
+  margin-top: 2rem;
+  @media (max-width: 789px) {
+    gap: 1rem;
+  }
+`;
+const Card = styled(motion.div)`
+  box-shadow: 0 0.5rem 1.5rem rgba(0, 0, 0, 0.1);
+  img {
+    width: 100%;
+    border-bottom-left-radius: 2rem;
+    border-bottom-right-radius: 2rem;
+    object-fit: cover;
+  }
+  h4 {
+    text-align: center;
+    color: #434343;
+    padding: 1rem;
+  }
+  p {
+    color: #27ae60;
+    text-align: center;
+    text-decoration: underline;
+  }
+`;
+export default Searched;
